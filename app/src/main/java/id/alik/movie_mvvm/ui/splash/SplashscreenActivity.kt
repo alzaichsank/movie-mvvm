2962011fb4ec4f36b50340f01ff38598ad86e9bc
Alza Ichsan K <alzaichsank@gmail.com>
2020-02-22 22:56:12 +0700

    add flow splashscreen to main

--- app/src/main/java/id/alik/movie_mvvm/ui/splash/SplashscreenActivity.kt
+++ app/src/main/java/id/alik/movie_mvvm/ui/splash/SplashscreenActivity.kt
@@ -1,7 +1,9 @@
 package id.alik.movie_mvvm.ui.splash
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import id.alik.movie_mvvm.R
 
 class SplashscreenActivity : AppCompatActivity() {
@@ -9,5 +11,13 @@ class SplashscreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splashscreen)
+        Handler().postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }, SPLASH_TIME)
+
+    }
+    companion object {
+        const val SPLASH_TIME: Long = 3000
     }
 }
